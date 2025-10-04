f9096b788508c294e82fd1a47e9d070d2f2077db
Will Paten <william.paten@gmail.com>
2025-10-04 09:34:59 -0400

    Set up a custom Helloworld and Footer Component.

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+// Styles have to be objects which is why there is teh double curly braces
+// The Returned color also has to be a string
+export default function Footer() {
+  return (
+    <div>
+      <p style={{ color: "red" }}>This is my Footer</p>
+    </div>
+  );
+}
